e9f2875e41523801345e58a7f4c0f9ede5bd3dbc
thiago-NSampaio <110494178+thiago-NSampaio@users.noreply.github.com>
2023-10-13 21:11:05 -0400

    Criação das views.

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require('express')
+const path = require('path')
 require('./config/database')
 
-const checkListRouter = require('./src/routes/checklist.js');
+const checkListRouter = require('./src/routes/checklist');
+const rootRouter = require('./src/routes/index');
+
 
 const app = express();
-
 app.use(express.json())
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.set('views', path.join(__dirname, 'src/views'));
+app.set('view engine', 'ejs');
+
+app.use('/',rootRouter)
 
 app.use('/checklists',checkListRouter)
 
