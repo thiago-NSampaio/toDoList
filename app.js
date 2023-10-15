d815ea52e91b18977f701d44919fd4a00c186201
thiago-NSampaio <110494178+thiago-NSampaio@users.noreply.github.com>
2023-10-15 18:31:29 -0400

    Crição das views de edit, form, index, new, show e error. Usando as rotas de acordo com seus métodos.

--- app.js
+++ app.js
@@ -4,10 +4,13 @@ require('./config/database')
 
 const checkListRouter = require('./src/routes/checklist');
 const rootRouter = require('./src/routes/index');
-
+const methodOverride = require('method-override');
 
 const app = express();
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method', {methods:['POST', 'GET']}));
+
 
 app.use(express.static(path.join(__dirname, 'public')))
 
--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -6,53 +6,78 @@ const Checklist = require('../models/checklist');
 
 router.get('/', async (req, res) => {
   try {
-      let checklists = await Checklist.find({})
-      res.status(200).json(checklists)
+      let checklists = await Checklist.find({}) // Encontra todas as checklists.
+      res.status(200).render('checklists/index', {checklists: checklists}) // Redireciona após a criação.
   } catch (error) {
-    res.status(500).json(error)
-  }
+    res.status(200).render('pages/error', {error: 'Erro ao exibir as Listas'}) // Renderiza a página de erro em caso de falha.
+}
 })
 
+
+
 router.post('/', async (req, res) => {
-    let {name} = req.body
-    
+    const { name } = req.body.checklist; // Extrai o nome do checklist do corpo da solicitação.
+    const checklist = new Checklist({ name }); // Cria uma instância de Checklist com o nome fornecido.
+
     try {
-        let checklist = await Checklist.create({ name })
-        res.status(200).json(checklist);
+        await checklist.save(); // Salve a instância no banco de dados.
+        res.redirect('/checklists'); // Redireciona após a criação.
     } catch (error) {
-        res.status(422).json(error);
+        res.status(422).render('pages/error', { error }); // Renderiza a página de erro em caso de falha.
     }
-    console.log(req.body);
+    console.log(req.body.checklist);
+});
+
+
+router.get('/new', async (req, res) => {
+    try {
+        let checklist = new Checklist();
+        res.status(200).render('checklists/new', {checklist: checklist})
+    } catch (error) {
+        res.status(500).render('pages/error', {erros: 'Erro ao carregar o formulário'})
+    }
 })
 
 router.get('/:id', async (req, res) => {
+    try {
+      let checklist = await Checklist.findById(req.params.id);
+      res.status(200).render('checklists/show', { checklist: checklist });
+    } catch (error) {
+      res.status(500).render('pages/error', { error: 'Erro ao exibir as Listas de tarefas' });
+    }
+  });
 
+router.get('/:id/edit', async (req, res) => {
     try {
         let checklist = await Checklist.findById(req.params.id)
-        res.status(200).json(checklist)
+        res.status(200).render('checklists/edit', {checklist: checklist})
     } catch (error) {
-        res.status(422).json(error);
+        res.status(200).render('pages/error', {error: 'Erro ao exibir a edição da Lista de tarefas'})
     }
 
 })
 
 router.put('/:id', async (req, res) => {
+    const { name } = req.body.checklist;
+    const checklist = await Checklist.findById(req.params.id);
 
     try {
-        let checklist = await Checklist.findByIdAndUpdate(req.params.id, { new: true });
-        res.status(200).json(checklist)
+        await checklist.updateOne({ name });
+        res.redirect('/checklists');
     } catch (error) {
-        res.status(422).json(error)
+        res.status(422).render('pages/error', { error }); // Use a variável 'error' para renderizar o erro.
     }
+    console.log(req.body.checklist);
+});
 
-})
+
 
 router.delete('/:id', async (req, res) => {
     try {
         let checklist = await Checklist.findByIdAndRemove(req.params.id)
-        res.status(200).json(checklist)
+        res.redirect('/checklists');
     } catch (error) {
-        res.status(422).json(error)
+        res.status(422).render('checklists/edit', {checklist: {...Checklist, errors}})
     }
 })
 
